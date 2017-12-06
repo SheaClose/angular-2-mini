ce144f41b5ed8a506154df6ab82e4140dd842bb8
SheaClose <SheaClose@gmail.com>
2017-12-06 13:44:35 -0600

    done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-angular.module('myApp', []);
+angular
+  .module("myApp", ["ui.router"])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: "./js/home/homeTmpl.html"
+      })
+      .state("products", {
+        url: "/products/:id",
+        templateUrl: "./js/products/productsTmpl.html",
+        controller: "productsCtrl",
+        resolve: {
+          products: function(productsSrvc, $state) {
+            return productsSrvc
+              .getProducts($state.params.id)
+              .then(res => res.data)
+              .catch(err => {
+                console.log(err);
+                $state.go("home");
+              });
+          }
+        }
+      })
+      .state("settings", {
+        url: "/settings?category",
+        templateUrl: "./js/settings/settingsTmpl.html",
+        controller: function($stateParams) {
+          console.log("$stateParams: ", $stateParams);
+        }
+      });
+    $urlRouterProvider.otherwise("/");
+  });
---
+++ js/products/productsCtrl.js
@@ -0,0 +1,3 @@
+angular.module("myApp").controller("productsCtrl", function($scope, products) {
+  console.log("products: ", products);
+});
